6f3020c92ee49a92bb1cf9fcbc073a0cbd459411
Sergey Pugachev <spugachev@gmail.com>
2017-08-30 18:19:03 +0300

    Login

--- samples/01-basics/utils.js
+++ samples/01-basics/utils.js
@@ -1,7 +1,9 @@
-export async function loadWasm(fileName){
-    const res = await fetch(`/${fileName}.wasm`);
-    const bytes = await res.arrayBuffer();
-    const module = await WebAssembly.compile(bytes);
-    const instance = await WebAssembly.instantiate(module);
-    return {instance, module}; 
+export class WashHelper {
+    async loadWasm(fileName){
+        const res = await fetch(`/${fileName}.wasm`);
+        const bytes = await res.arrayBuffer();
+        const module = await WebAssembly.compile(bytes);
+        const instance = await WebAssembly.instantiate(module);
+        return {instance, module}; 
+    } 
 }
